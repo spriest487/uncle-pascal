25513115890cd6314144c429310ce922663ae00f
spriest487 <spriest487@googlemail.com>
2018-08-16 20:56:34 +0000

    Reuse tokenizer regexes between runs

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -2,7 +2,10 @@ use std::{
     fmt,
     rc::*,
 };
-use regex;
+use regex::{
+    Regex,
+    Captures,
+};
 
 use tokens;
 use operators;
@@ -28,11 +31,11 @@ impl fmt::Display for IllegalToken {
 
 enum TokenMatchParser {
     Simple(tokens::Token),
-    ParseFn(Box<Fn(&regex::Captures, &CompileOptions) -> Option<tokens::Token>>),
+    ParseFn(Box<Fn(&Captures, &CompileOptions) -> Option<tokens::Token>>),
 }
 
 impl TokenMatchParser {
-    fn try_parse(&self, token_match: &regex::Captures, opts: &CompileOptions) -> Option<tokens::Token> {
+    fn try_parse(&self, token_match: &Captures, opts: &CompileOptions) -> Option<tokens::Token> {
         match self {
             &TokenMatchParser::Simple(ref token) => Some(token.clone()),
             &TokenMatchParser::ParseFn(ref parse_fn) => parse_fn(token_match, opts),
@@ -40,7 +43,140 @@ impl TokenMatchParser {
     }
 }
 
-fn parse_literal_string(token_match: &regex::Captures, _opts: &CompileOptions) -> Option<tokens::Token> {
+struct Tokenizer<'a> {
+    all_tokens_regex: Regex,
+    token_matchers: Vec<(Regex, TokenMatchParser)>,
+    opts: &'a CompileOptions,
+}
+
+impl<'a> Tokenizer<'a> {
+    pub fn new(opts: &'a CompileOptions) -> Self {
+        let token_patterns = vec![
+            func_pattern(r"[a-zA-Z_](([a-zA-Z0-9_])?)+", parse_name),
+            //anything between two quote marks, with double quote as literal quote mark
+            func_pattern(r"'(([^']|'{2})*)'", parse_literal_string),
+            func_pattern(r"-?[0-9]+", parse_literal_integer), // decimal int
+            func_pattern(r"#[0-9]{1,3}", parse_literal_integer), // char
+            func_pattern(r"\$([0-9A-Fa-f]+)", parse_literal_integer), //hex int
+            simple_pattern(r"\*", operators::Multiply),
+            simple_pattern(r"[/]", operators::Divide),
+            simple_pattern(r"\(", tokens::BracketLeft),
+            simple_pattern(r"\)", tokens::BracketRight),
+            simple_pattern(r"\[", tokens::SquareBracketLeft),
+            simple_pattern(r"\]", tokens::SquareBracketRight),
+            simple_pattern(r":=", operators::Assignment),
+            simple_pattern(r">=", operators::Gte),
+            simple_pattern(r">", operators::Gt),
+            simple_pattern(r"<>", operators::NotEquals),
+            simple_pattern(r"<=", operators::Lte),
+            simple_pattern(r"<", operators::Lt),
+            simple_pattern(r"@", operators::AddressOf),
+            simple_pattern(r"\+", operators::Plus),
+            simple_pattern(r"\-", operators::Minus),
+            simple_pattern("=", operators::Equals),
+            simple_pattern(r"\^", operators::Deref),
+            simple_pattern(r":", tokens::Colon),
+            simple_pattern(r";", tokens::Semicolon),
+            simple_pattern(r",", tokens::Comma),
+            simple_pattern(r"\.", tokens::Period),
+        ];
+
+        /* make a combined regex of all the possible token match patterns */
+        let all_tokens_pattern = {
+            let pattern = token_patterns.iter()
+                .map(|(pattern, _)| pattern.clone())
+                .collect::<Vec<_>>()
+                .join("|");
+            let and_unrecognized = "|\\s+|.";
+            if opts.case_sensitive() {
+                pattern + and_unrecognized
+            } else {
+                format!("(?i){}{}", pattern, and_unrecognized)
+            }
+        };
+
+        let all_tokens_regex = Regex::new(&all_tokens_pattern)
+            .expect("tokens regex must be valid");
+
+        /* compile token pattern regexes */
+        let token_matchers = token_patterns.into_iter()
+            .map(|(pattern, token_parser)| {
+                let whole_token_pattern = if opts.case_sensitive() {
+                    format!("^{}$", pattern)
+                } else {
+                    format!("(?i)^{}$", pattern)
+                };
+
+                let token_regex = Regex::new(&whole_token_pattern)
+                    .expect("token regex must be valid");
+
+                (token_regex, token_parser)
+            })
+            .collect::<Vec<_>>();
+
+        Tokenizer {
+            all_tokens_regex,
+            token_matchers,
+            opts,
+        }
+    }
+}
+
+impl<'a> Tokenizer<'a> {
+    fn parse_line(&self,
+                  file_name: &str,
+                  line_num: usize,
+                  line: &str) -> TokenizeResult<Vec<source::Token>> {
+        let file_name_shared = Rc::new(String::from(file_name));
+
+        let parsed_tokens = self.all_tokens_regex.captures_iter(&line)
+            .filter_map(|capture| {
+                let token_match = capture.get(0).unwrap();
+                let token_source = token_match.as_str().trim();
+                let col = token_match.start() + 1;
+
+                if token_source.len() == 0 {
+                    /* token is purely whitespace, ignore it */
+                    None
+                } else {
+                    let first_matching_token = self.token_matchers.iter()
+                        .find(|pattern| {
+                            pattern.0.is_match(&token_source)
+                        })
+                        .and_then(|pattern| {
+                            //match against this regex only to get the expected groups
+                            let pattern_captures = pattern.0.captures(&token_source)
+                                .unwrap();
+
+                            /* a pattern matched this token, use the parsing
+                            function associated with this pattern */
+                            pattern.1.try_parse(&pattern_captures, self.opts)
+                        })
+                        .map(|token| source::Token {
+                            token: Rc::from(token),
+                            location: source::Location {
+                                file: file_name_shared.clone(),
+                                line: line_num,
+                                col,
+                            },
+                        })
+                        .ok_or_else(|| IllegalToken {
+                            text: token_source.to_owned(),
+                            file: String::from(file_name),
+                            col,
+                            line: line_num,
+                        });
+
+                    Some(first_matching_token)
+                }
+            })
+            .collect::<TokenizeResult<_>>()?;
+
+        Ok(parsed_tokens)
+    }
+}
+
+fn parse_literal_string(token_match: &Captures, _opts: &CompileOptions) -> Option<tokens::Token> {
     //group 1 must be the inner content of the string (eg, minus the outer quotes)
     let text = token_match.get(1).unwrap().as_str().to_owned()
         .replace("''", "'");
@@ -48,7 +184,7 @@ fn parse_literal_string(token_match: &regex::Captures, _opts: &CompileOptions) -
     Some(tokens::LiteralString(text))
 }
 
-fn parse_literal_integer(token_match: &regex::Captures,
+fn parse_literal_integer(token_match: &Captures,
                          _opts: &CompileOptions)
                          -> Option<tokens::Token> {
     let int_text = token_match.get(0).unwrap().as_str();
@@ -58,7 +194,7 @@ fn parse_literal_integer(token_match: &regex::Captures,
     Some(tokens::LiteralInteger(val))
 }
 
-fn parse_name(token_match: &regex::Captures, opts: &CompileOptions) -> Option<tokens::Token> {
+fn parse_name(token_match: &Captures, opts: &CompileOptions) -> Option<tokens::Token> {
     let text = token_match.get(0).unwrap().as_str().to_owned();
 
     Some(keywords::Keyword::try_parse(&text, opts)
@@ -75,130 +211,12 @@ fn simple_pattern<T>(pattern: &str, token: T) -> (String, TokenMatchParser)
 }
 
 fn func_pattern<TFn>(pattern: &str, f: TFn) -> (String, TokenMatchParser)
-    where TFn: Fn(&regex::Captures, &CompileOptions) -> Option<tokens::Token> + 'static
+    where TFn: Fn(&Captures, &CompileOptions) -> Option<tokens::Token> + 'static
 {
     (pattern.to_owned(), TokenMatchParser::ParseFn(Box::new(f)))
 }
 
-fn token_patterns() -> Vec<(String, TokenMatchParser)> {
-    vec![
-        func_pattern(r"[a-zA-Z_](([a-zA-Z0-9_])?)+", parse_name),
-        //anything between two quote marks, with double quote as literal quote mark
-        func_pattern(r"'(([^']|'{2})*)'", parse_literal_string),
-        func_pattern(r"-?[0-9]+", parse_literal_integer), // decimal int
-        func_pattern(r"#[0-9]{1,3}", parse_literal_integer), // char
-        func_pattern(r"\$([0-9A-Fa-f]+)", parse_literal_integer), //hex int
-        simple_pattern(r"\*", operators::Multiply),
-        simple_pattern(r"[/]", operators::Divide),
-        simple_pattern(r"\(", tokens::BracketLeft),
-        simple_pattern(r"\)", tokens::BracketRight),
-        simple_pattern(r"\[", tokens::SquareBracketLeft),
-        simple_pattern(r"\]", tokens::SquareBracketRight),
-        simple_pattern(r":=", operators::Assignment),
-        simple_pattern(r">=", operators::Gte),
-        simple_pattern(r">", operators::Gt),
-        simple_pattern(r"<>", operators::NotEquals),
-        simple_pattern(r"<=", operators::Lte),
-        simple_pattern(r"<", operators::Lt),
-        simple_pattern(r"@", operators::AddressOf),
-        simple_pattern(r"\+", operators::Plus),
-        simple_pattern(r"\-", operators::Minus),
-        simple_pattern("=", operators::Equals),
-        simple_pattern(r"\^", operators::Deref),
-        simple_pattern(r":", tokens::Colon),
-        simple_pattern(r";", tokens::Semicolon),
-        simple_pattern(r",", tokens::Comma),
-        simple_pattern(r"\.", tokens::Period),
-    ]
-}
-
 pub type TokenizeResult<T> = Result<T, IllegalToken>;
-
-fn parse_line(file_name: &str,
-              line_num: usize,
-              line: &str,
-              opts: &CompileOptions) -> TokenizeResult<Vec<source::Token>> {
-    /* make a combined regex of all the possible token match patterns */
-    let all_tokens_pattern = {
-        let pattern = token_patterns().into_iter()
-            .map(|(pattern, _)| pattern)
-            .collect::<Vec<_>>()
-            .join("|");
-
-        let and_unrecognized = "|\\s+|.";
-        if opts.case_sensitive() {
-            pattern + and_unrecognized
-        } else {
-            format!("(?i){}{}", pattern, and_unrecognized)
-        }
-    };
-
-    let all_tokens_regex = regex::Regex::new(&all_tokens_pattern)
-        .expect("tokens regex must be valid");
-
-    /* compile token pattern regexes */
-    let patterns = token_patterns().into_iter()
-        .map(|(pattern, token_parser)| {
-            let whole_token_pattern = if opts.case_sensitive() {
-                format!("^{}$", pattern)
-            } else {
-                format!("(?i)^{}$", pattern)
-            };
-
-            let token_regex = regex::Regex::new(&whole_token_pattern)
-                .expect("token regex must be valid");
-
-            (token_regex, token_parser)
-        })
-        .collect::<Vec<_>>();
-
-    let file_name_shared = Rc::new(String::from(file_name));
-
-    let parsed_tokens = all_tokens_regex.captures_iter(&line)
-        .filter_map(|capture| {
-            let token_match = capture.get(0).unwrap();
-            let token_source = token_match.as_str().trim();
-            let col = token_match.start() + 1;
-
-            if token_source.len() == 0 {
-                /* token is purely whitespace, ignore it */
-                None
-            } else {
-                let first_matching_token = patterns.iter()
-                    .find(|pattern| {
-                        pattern.0.is_match(&token_source)
-                    })
-                    .and_then(|pattern| {
-                        //match against this regex only to get the expected groups
-                        let pattern_captures = pattern.0.captures(&token_source)
-                            .unwrap();
-
-                        /* a pattern matched this token, use the parsing
-                        function associated with this pattern */
-                        pattern.1.try_parse(&pattern_captures, opts)
-                    })
-                    .map(|token| source::Token {
-                        token: Rc::from(token),
-                        location: source::Location {
-                            file: file_name_shared.clone(),
-                            line: line_num,
-                            col,
-                        },
-                    })
-                    .ok_or_else(|| IllegalToken {
-                        text: token_source.to_owned(),
-                        file: String::from(file_name),
-                        col,
-                        line: line_num,
-                    });
-
-                Some(first_matching_token)
-            }
-        })
-        .collect::<TokenizeResult<_>>()?;
-
-    Ok(parsed_tokens)
-}
 
 pub fn tokenize(file_name: &str,
                 source: &str,
@@ -208,9 +226,12 @@ pub fn tokenize(file_name: &str,
         .map(str::to_owned)
         .collect();
 
+    let tokenizer = Tokenizer::new(opts);
     let parsed_lines: Vec<_> = lines.into_iter()
         .enumerate()
-        .map(|(line_num, line)| parse_line(file_name, line_num + 1, &line, opts))
+        .map(|(line_num, line)| {
+            tokenizer.parse_line(file_name, line_num + 1, &line)
+        })
         .collect::<TokenizeResult<_>>()?;
 
     if parsed_lines.len() == 0 {
