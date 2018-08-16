18ee44608b946ac3a422ba9db7a665ac48a14cde
spriest487 <spriest487@googlemail.com>
2018-08-16 20:56:34 +0000

    Adding more of the SDL2 interface

--- units/SDL.pas
+++ units/SDL.pas
@@ -27,22 +27,48 @@ const
     INIT_GAMECONTROLLER =       $00002000
     INIT_EVENTS =               $00004000
     INIT_NOPARACHUTE =          $00100000
+
     WINDOWPOS_UNDEFINED =       $1FFF0000
 
-function Init(flags: UInt32): Int32
-procedure Quit
+    EVENT_QUIT = $100
+    EVENT_LASTEVENT = $FFFF
 
-function CreateWindow(
-    title: ^Byte;
+type
+    EventType = (
+        EVENT_QUIT,
+        EVENT_LASTEVENT
+    )
+
+    Event = record
+        case Type: EventType of
+            EVENT_QUIT: ()
+            EVENT_LASTEVENT: (Padding: array[0..56] of Byte)
+    end
+
+function Init(flags: UInt32): Int32
+    ; external name 'SDL_Init'
+    ; cdecl
+
+procedure Quit
+    ; external name 'SDL_Quit'
+
+function CreateWindow(title: ^Byte;
     x, y, width, height: Int32;
-    flags: UInt32
-): Pointer
+    flags: UInt32): Pointer
+    ; external name 'SDL_CreateWindow'
+    ; cdecl
 
 procedure DestroyWindow(window: Pointer)
+    ; external name 'SDL_DestroyWindow'
+    ; cdecl
 
 function PollEvent(event: Pointer): Int32
+    ; external name 'SDL_PollEvent'
+    ; cdecl
 
-procedure Delay(ms: UInt32)
+procedure Delay(ms: UInt32); 
+    external name 'SDL_Delay'
+    ; cdecl
 
 implementation
 end.
