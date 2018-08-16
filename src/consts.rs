218bf3b9e37e61ed84bd7b0b6282ca3dc2b906af
spriest487 <spriest487@googlemail.com>
2018-08-16 20:56:34 +0000

    From impls for IntConstant

--- src/consts.rs
+++ src/consts.rs
@@ -43,35 +43,56 @@ impl IntConstant {
         let first_char = s.chars().next()?;
 
         match first_char {
-            /* hex literals always produce unsigned values */
             '$' => {
                 let val = u64::from_str_radix(&s[1..], 16).ok()?;
+
+                /* hex literals always produce unsigned values */
                 if val <= u32::max_value() as u64 {
                     Some(IntConstant::U32(val as u32))
                 } else {
                     Some(IntConstant::U64(val))
                 }
             }
+
             /* negative numbers produce int32, or uint32 if they're too large */
             '-' => {
                 let val: i64 = s.parse().ok()?;
-                if val >= i32::min_value() as i64 {
-                    Some(IntConstant::I32(val as i32))
-                } else {
-                    Some(IntConstant::I64(val))
-                }
+                Some(IntConstant::from(val))
             }
+
             /* positive numbers produce int32, or i64/u64 if they're too large */
             _ => {
                 let val: u64 = s.parse().ok()?;
-                if val <= i32::max_value() as u64 {
-                    Some(IntConstant::I32(val as i32))
-                } else if val <= i64::max_value() as u64 {
-                    Some(IntConstant::I64(val as i64))
-                } else {
-                    Some(IntConstant::U64(val))
-                }
+                Some(IntConstant::from(val))
             }
+        }
+    }
+}
+
+impl From<i32> for IntConstant {
+    fn from(val: i32) -> Self {
+        IntConstant::I32(val)
+    }
+}
+
+impl From<i64> for IntConstant {
+    fn from(val: i64) -> Self {
+        if val >= i32::min_value() as i64 {
+            IntConstant::I32(val as i32)
+        } else {
+            IntConstant::I64(val)
+        }
+    }
+}
+
+impl From<u64> for IntConstant {
+    fn from(val: u64) -> Self {
+        if val <= i32::max_value() as u64 {
+            IntConstant::I32(val as i32)
+        } else if val <= i64::max_value() as u64 {
+            IntConstant::I64(val as i64)
+        } else {
+            IntConstant::U64(val)
         }
     }
 }
