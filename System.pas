0337cfa8ea8c72eb898266d6e7c987e11835dbb6
spriest487 <spriest487@googlemail.com>
2018-08-16 20:56:34 +0000

    Use Pascal implementation of StringConcat

--- System.pas
+++ System.pas
@@ -4,32 +4,32 @@ interface
 
 implementation
 
-//constructor StringCreate(): String
-//begin
-//    result.Chars := nil
-//    result.Length := 0
-//end
-//
-//function StringConcat(a: String; b: String): String
-//begin
-//    if a.Length == 0 and b.Length == 0 then
-//        result := StringCreate()
-//    else if emptyA then
-//        result := StringFromBytes(b.Chars, b.Length)
-//    else if emptyB then
-//        result := StringFromBytes(a.Chars, a.Length)
-//    else begin
-//        result := StringCreate();
-//
-//        result.Length := a.Length + b.Length;
-//        result.Chars := GetMem(totalLen);
-//
-//        for c := 0 to a.Length do
-//            ^(result.Chars + c) := ^(a.Chars + c);
-//
-//        for c := 0 to b.Length do
-//            ^(result.Chars + a.Length + c) := (b.Chars + c);
-//    end;
-//end
+constructor StringCreate(): String
+begin
+    result.Chars := nil
+    result.Length := 0
+end
+
+function StringConcat(a: String; b: String): String
+begin
+    if a.Length = 0 and b.Length = 0 then
+        result := StringCreate()
+    else if a.Length = 0 then
+        result := StringFromBytes(b.Chars, b.Length)
+    else if b.Length = 0 then
+        result := StringFromBytes(a.Chars, a.Length)
+    else begin
+        result := StringCreate();
+
+        result.Length := a.Length + b.Length;
+        result.Chars := GetMem(result.Length);
+
+        for let c := 0 to a.Length do
+            ^(result.Chars + c) := ^(a.Chars + c);
+
+        for let c := 0 to b.Length do
+            ^(result.Chars + a.Length + c) := ^(b.Chars + c);
+    end;
+end
 
 end.
