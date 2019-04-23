884086ae5fb242ec54bd4bd92949adeeaa437cb1
spriest487 <spriest487@googlemail.com>
2019-04-23 01:24:00 +0100

    Use unqualified names in System.pas

--- units/System.pas
+++ units/System.pas
@@ -22,16 +22,16 @@ begin
     self.len := 0;
 end;
 
-function StringConcat(a, b: System.String): System.String
+function StringConcat(a, b: String): String
 begin
     if a.len = 0 and b.len = 0 then
-        System.String(chars: nil; len: 0)
+        String(chars: nil; len: 0)
     else if a.len = 0 then b
     else if b.len = 0 then a
     else begin
         let len := a.len + b.len;
 
-        let bytes := System.GetMem(len);
+        let bytes := GetMem(len);
 
         for let i := 0 to a.len - 1 do
             (bytes + i)^ := (a.chars + i)^;
@@ -39,6 +39,6 @@ begin
         for let i := 0 to b.len - 1 do
             (bytes + a.len + i)^ := (b.chars + i)^;
 
-        System.String(chars: bytes; len: len);
+        String(chars: bytes; len: len);
     end
 end;
