63780138a890ff241297de41bf95924825fb5c83
spriest487 <spriest487@googlemail.com>
2020-01-27 23:18:03 +0000

    QuickSort for string arrays

--- demos/collections/ArraySort.pas
+++ demos/collections/ArraySort.pas
@@ -4,6 +4,18 @@ var foods: array of String := [
     'apple',
     'melon',
     'yogurt',
+    'cookies',
+    'treacle',
+    'onion',
+    'banana',
+    'mustard',
+    'lemon',
 ];
 
 QuickSort(foods);
+
+let len := Length of String(foods);
+
+for let i := 0 to len - 1 do begin
+    WriteLn(foods[i]);
+end;
--- units/Arrays.pas
+++ units/Arrays.pas
@@ -1,7 +1,55 @@
 uses System;
 
-export function QuickSort(x: array of String)
+function QuickSortPartition(arr: array of String; left, right: Integer): Integer
 begin
-    var i: Integer := 0;
-    var j: Integer := Length of String(x) - 1;
+    var l := left;
+    var r := right;
+
+    let pivot := arr[l];
+    var result := r;
+
+    while true do begin
+        while CompareStr(arr[l], pivot) < 0 do begin
+            l := l + 1;
+        end;
+        while CompareStr(arr[r], pivot) > 0 do begin
+            r := r - 1;
+        end;
+
+        if l < r then begin
+            if CompareStr(arr[l], arr[r]) = 0 then begin
+                result := r;
+                break;
+            end;
+
+            let temp := arr[l];
+            arr[l] := arr[r];
+            arr[r] := temp;
+        end
+        else begin
+            result := r;
+            break;
+        end;
+    end;
+
+    result
+end;
+
+function QuickSortRange(arr: array of String; left, right: Integer)
+begin
+    if left < right then begin
+        let pivot := QuickSortPartition(arr, left, right);
+
+        if pivot > 1 then begin
+            QuickSortRange(arr, left, pivot - 1);
+        end
+        else if pivot + 1 < right then begin
+            QuickSortRange(arr, pivot + 1, right);
+        end;
+    end;
+end;
+
+export function QuickSort(arr: array of String)
+begin
+    QuickSortRange(arr, 0, Length of String(arr) - 1);
 end;
