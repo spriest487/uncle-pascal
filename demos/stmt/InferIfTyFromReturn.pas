ecd3c43515fa81b9b42b4f7321e28f37ba97d2b2
spriest487 <spriest487@googlemail.com>
2021-11-11 01:10:08 +0000

    Demo tidy up

--- demos/stmt/InferIfTyFromReturn.pas
+++ demos/stmt/InferIfTyFromReturn.pas
@@ -1,8 +1,12 @@
 uses System;
 
-function InferResultTy(i: Integer): Option of Integer
+function InferResultTy(i: Integer): Option[Integer]
 begin
     if i > 1 then Option.Some(i) else Option.None()
 end;
 
 let x := InferResultTy(2);
+
+WriteLn('x is: ' + if x is Option.Some xVal
+    then xVal.ToString()
+    else 'Nothing');
