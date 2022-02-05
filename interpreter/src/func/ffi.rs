a2cc439a58f5fac025fa3d13b7e73201f2ce80a3
spriest487 <spriest487@googlemail.com>
2022-02-05 12:32:37 +0000

    Smallvec for less allocations for small function calls

--- interpreter/src/func/ffi.rs
+++ interpreter/src/func/ffi.rs
@@ -1,15 +1,11 @@
-use std::{ptr::slice_from_raw_parts};
-use std::ffi::c_void;
-use std::ptr::null_mut;
-use crate::{
-    ExecResult, Interpreter,
-    marshal::ForeignType,
-};
-use ::libffi::{
-    middle::Cif,
-    raw::ffi_call as ffi_raw_call,
-};
+use crate::{marshal::ForeignType, ExecResult, Interpreter};
+use ::libffi::{middle::Cif, raw::ffi_call as ffi_raw_call};
 use pas_ir::{LocalID, Ref, Type};
+use smallvec::*;
+use std::{
+    ffi::c_void,
+    ptr::{null_mut, slice_from_raw_parts},
+};
 
 pub struct FfiInvoker {
     cif: Cif,
@@ -28,7 +24,7 @@ impl FfiInvoker {
         symbol: *const (),
         ffi_param_tys: Vec<ForeignType>,
         return_ty: Type,
-        ffi_return_ty: ForeignType
+        ffi_return_ty: ForeignType,
     ) -> Self {
         Self {
             cif,
@@ -45,27 +41,32 @@ impl FfiInvoker {
         // marshal args into a byte vec - we need to pass pointers into this vec, so it can't
         // be reallocated, and we need to calculate the total size now
         let params_total_size = self.ffi_param_tys.iter().map(|p| p.size()).sum();
+        let param_count = self.ffi_param_tys.len();
 
-        let mut args = vec![0; params_total_size].into_boxed_slice();
-        let mut args_ptrs = vec![null_mut(); self.ffi_param_tys.len()].into_boxed_slice();
+        let mut args: SmallVec<[u8; 64]> = smallvec![0; params_total_size];
+        let mut args_ptrs: SmallVec<[*mut c_void; 4]> = smallvec![null_mut(); param_count];
 
-        let first_param_local = match self.return_ty { Type::Nothing => 0, _ => 1 };
+        let first_param_local = match self.return_ty {
+            Type::Nothing => 0,
+            _ => 1,
+        };
 
         let mut arg_offset = 0;
 
-        for i in 0..self.ffi_param_tys.len() {
+        for i in 0..param_count {
             let local_id = LocalID(first_param_local + i);
 
             let param_size = self.ffi_param_tys[i].size();
 
             let arg_val = state.load(&Ref::Local(local_id))?;
-            let bytes_copied = state.marshaller().marshal(&arg_val, &mut args[arg_offset..])?;
+            let bytes_copied = state
+                .marshaller()
+                .marshal(&arg_val, &mut args[arg_offset..])?;
 
             assert_eq!(bytes_copied, param_size);
 
-            args_ptrs[i] = unsafe {
-                (args.as_mut_ptr() as *mut c_void).offset(arg_offset as isize)
-            };
+            args_ptrs[i] =
+                unsafe { (args.as_mut_ptr() as *mut c_void).offset(arg_offset as isize) };
             arg_offset += param_size;
         }
 
@@ -89,7 +90,9 @@ impl FfiInvoker {
         if self.return_ty != Type::Nothing {
             unsafe {
                 let result_slice = slice_from_raw_parts(result_buf.as_ptr(), result_buf.len());
-                let return_val = state.marshaller().unmarshal(result_slice.as_ref().unwrap(), &self.return_ty)?;
+                let return_val = state
+                    .marshaller()
+                    .unmarshal(result_slice.as_ref().unwrap(), &self.return_ty)?;
 
                 let return_local = Ref::Local(LocalID(0));
                 state.store(&return_local, return_val.value)?;
