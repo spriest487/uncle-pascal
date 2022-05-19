0619f8ece2fa8eb8c8eae7881bfa9d1f8e63f38d
spriest487 <spriest487@googlemail.com>
2022-05-19 01:33:57 +0100

    Empty windows unit

---
+++ units/Windows.pas
@@ -0,0 +1,15 @@
+unit Windows;
+
+interface
+
+type
+    WORD = UInt16;
+    DWORD = UInt32;
+    HRESULT = UInt32;
+    HANDLE = Pointer;
+    HWND = Pointer;
+    HDC = Pointer;
+
+implementation
+
+end.
