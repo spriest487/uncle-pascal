f3b6c6fefa8a3b45c476223908d968558e415a8c
spriest487 <spriest487@googlemail.com>
2022-05-20 02:59:42 +0100

    Fix array demo

--- demos/types/Array.pas
+++ demos/types/Array.pas
@@ -1,5 +1,5 @@
 uses System;
-{
+
 var nums: array[3] of Integer := [1, 2, 3];
 nums[0] := 999;
 
@@ -19,11 +19,9 @@ for var i := 0 to 2 do begin
 end;
 
 WriteLn('');
-}
 
 var notEnoughNums: array[3] of Integer := [1, 999];
-var y := notEnoughNums[4 as Integer];
-{
-for var i := 0 to 3 do begin
+
+for var i := 0 to 2 do begin
     WriteLn('notEnoughNums ' + i + ' = ' + notEnoughNums[i]);
-end;}
+end;
