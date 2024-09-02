44e6571fa195da14ba7cecee5794d50f76bff4c2
spriest487 <spriest487@googlemail.com>
2024-09-02 02:53:09 +0100

    Updating old demos

--- demos/closure/Capture.pas
+++ demos/closure/Capture.pas
@@ -1,22 +1,19 @@
 unit Capture;
 
 implementation
-
-uses System;
-
-function GetAdder(a: Integer): function(Integer): Integer;
-begin
-    function(b: Integer): Integer;
+    uses System;
+    
+    function GetAdder(a: Integer): function(Integer): Integer;
     begin
-        a + b
-    end
-end;
+        function(b: Integer): Integer;
+        begin
+            a + b
+        end
+    end;
 
 initialization
-
-var add4 := GetAdder(4);
-var z := add4(3);
-
-WriteLn(z.ToString());
-
+    var add4 := GetAdder(4);
+    var z := add4(3);
+    
+    WriteLn(z.ToString());
 end.
--- demos/functions/UfcsArray.pas
+++ demos/functions/UfcsArray.pas
@@ -1,19 +1,21 @@
 uses System;
 
-function Array3Identity(a: array[3] of Integer): array[3] of Integer
-begin
-    a
-end;
-
-function DynArrayIdentity(a: array of Integer): array of Integer
-begin
-    a
-end;
-
-
-let a1 := Array3Identity([1, 2, 3]);
-let a2 := [1, 2, 3].Array3Identity();
-
-let a3 := DynArrayIdentity([1, 2, 3]);
-let a4: array of Integer := [1, 2, 3];
-let a5 := a4.DynArrayIdentity();
+initialization
+    function Array3Identity(a: array[3] of Integer): array[3] of Integer;
+    begin
+        a
+    end;
+    
+    function DynArrayIdentity(a: array of Integer): array of Integer;
+    begin
+        a
+    end;
+    
+    
+    var a1 := Array3Identity([1, 2, 3]);
+    var a2 := [1, 2, 3].Array3Identity();
+    
+    var a3 := DynArrayIdentity([1, 2, 3]);
+    var a4: array of Integer := [1, 2, 3];
+    var a5 := a4.DynArrayIdentity();
+end
