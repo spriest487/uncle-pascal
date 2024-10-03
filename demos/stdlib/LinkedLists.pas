d6e035973c09b931f016b9aa249b034d694b9973
spriest487 <spriest487@googlemail.com>
2024-10-03 17:07:00 +0100

    Update linked list demo

--- demos/stdlib/LinkedLists.pas
+++ demos/stdlib/LinkedLists.pas
@@ -7,17 +7,17 @@ initialization
 
     WriteLn('len of x: ' + x.Length());
 
-    x.Append(123);
-    x.Append(456);
+    x.Add(123);
+    x.Add(456);
     
     WriteLn('len of x: ' + x.Length());
 
-    if x.Nth(0) is Option.Some n then
+    if x.TryGet(0) is Option.Some n then
         WriteLn('x[0]: ' + n)
     else
         WriteLn('missing item 0! should be 123');
     
-    if x.Nth(1) is Option.Some n then
+    if x.TryGet(1) is Option.Some n then
         WriteLn('x[1]: ' + n)
     else
         WriteLn('missing item 0! should be 456');
