67a49f009658bd5197d1fc5d6500e15dea40d2b3
spriest487 <spriest487@googlemail.com>
2024-10-10 01:43:08 +0100

    Fix Result demo

--- demos/stdlib/Result.pas
+++ demos/stdlib/Result.pas
@@ -5,15 +5,15 @@ initialization
     var initial: Result[Integer, String] := Result.Ok(123);
     
     var multiplier := 2;
-    
-    var plusOne := initial.Then(
-        function(val: Integer): Result[Integer, String]; 
-        begin
-            Result.Ok(val + 1)
-        end
-    );
 
-    var finalResult := plusOne.Then(lambda val: Result.Ok(val * multiplier));
+    var finalResult := initial
+        .Then(
+            function(val: Integer): Result[Integer, String]; 
+            begin
+                Result.Ok(val + 1)
+            end
+        )
+        .Then[Integer](lambda val: Result.Ok(val * multiplier));
     
     match finalResult of
         Result.Ok val: WriteLn('OK: ' + val);
