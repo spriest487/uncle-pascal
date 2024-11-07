d34bb4ca0c3bdd0398b79304a33b4ae7143d56f2
spriest487 <spriest487@googlemail.com>
2024-11-07 01:00:39 +0000

    Updating demos that used non-interface `Self`

--- demos/typedecl/RecClass.pas
+++ demos/typedecl/RecClass.pas
@@ -4,16 +4,14 @@ uses System;
 { can't construct without an instance of itself }
 type ValidButImpossible = class
     self: ValidButImpossible;
-    self2: Self;
 end;
 
 type Rec = class
     self: ^Rec;
-    self2: ^Self;
 end;
 
 initialization
     var uninit: ValidButImpossible;
     
-    var rec: Rec := Rec(self: nil; self2: nil);
+    var rec: Rec := Rec(self: nil);
 end
